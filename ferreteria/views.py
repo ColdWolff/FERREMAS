700d2a758f0b2967809add7a197a9a722dd1bece
Aidan Asher <ai.arenas@duocuc.cl>
2024-04-27 17:48:37 -0400

    1 API Functional

--- ferreteria/views.py
+++ ferreteria/views.py
@@ -1,5 +1,4 @@
 from . import views
-from . import converter
 from datetime import datetime
 import bcchapi, requests, json
 from urllib.request import urlopen
@@ -14,31 +13,70 @@ def fecha_de_hoy():
     return seteo_fecha
 
 def usd_a_clp(usd):
+    values = []
     #url = 'https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F073.TCO.PRE.Z.D&firstdate='+str(fecha_de_hoy())+'&lastdate='+str(fecha_de_hoy())
     url = 'https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F073.TCO.PRE.Z.D&firstdate=2024-04-26&lastdate=2024-04-26'
     response = urlopen(url)
     data = json.load(response)
     valor = data["Series"]["Obs"][0]["value"]
     clp = float(usd)*float(valor)
-    return clp
+    values.append(valor)
+    values.append(clp)
+    return values
+
+def euro_a_clp(euro):
+    values = []
+    #url = 'https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F072.CLP.EUR.N.O.D&firstdate='+str(fecha_de_hoy())+'&lastdate='+str(fecha_de_hoy())
+    url = 'https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F072.CLP.EUR.N.O.D&firstdate=2024-04-26&lastdate=2024-04-26'
+    response = urlopen(url)
+    data = json.load(response)
+    valor = data["Series"]["Obs"][0]["value"]
+    clp = float(euro)*float(valor)
+    values.append(valor)
+    values.append(clp)
+    return values
+
+def libra_a_clp(libra):
+    values = []
+    #url = 'https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F072.CLP.GBP.N.O.D&firstdate='+str(fecha_de_hoy())+'&lastdate='+str(fecha_de_hoy())
+    url = 'https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F072.CLP.GBP.N.O.D&firstdate=2024-04-26&lastdate=2024-04-26'
+    response = urlopen(url)
+    data = json.load(response)
+    valor = data["Series"]["Obs"][0]["value"]
+    clp = float(libra)*float(valor)
+    values.append(valor)
+    values.append(clp)
+    return values
 
 def convertir(request):
     if request.method == "GET":
         return render(request, "llama_conversor.html")
     elif request.method == "POST":
-        usd_str = request.POST.get("usd")
-        if not usd_str:
+        value_str = request.POST.get("usd")
+        tipo_cambio = request.POST.get("moneda")
+        if not value_str:
             return render(request, "llama_conversor.html", {"mensaje": "El campo no puede estar vacío."})        
         try:
             #Verifica Numero
-            usd = float(usd_str)
+            value = float(value_str)
         except ValueError:
             return render(request, "llama_conversor.html", {"mensaje": "Por favor, ingrese un número válido."})
-        if usd < 0:
+        if value < 0:
             #Verifica Positivo
             return render(request, "llama_conversor.html", {"mensaje": "El número no puede ser negativo."})  
-        clp = usd_a_clp(usd)
-        return render(request, "converter.html", {"clp": clp})
+        if tipo_cambio == "USD":
+            values = usd_a_clp(value)
+            actual = values[0]
+            clp = values[1]
+        elif tipo_cambio == "Euro":
+            values = euro_a_clp(value)
+            actual = values[0]
+            clp = values[1]
+        elif tipo_cambio == "Libra":
+            values = libra_a_clp(value)
+            actual = values[0]
+            clp = values[1]
+        return render(request, "converter.html", {"clp": clp,"actual": actual})
     else:
         return HttpResponse(status=405)
 
