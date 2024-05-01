e83a44a9eb9650888f43272af59f2319336dc905
Aidan Asher <ai.arenas@duocuc.cl>
2024-04-30 22:09:06 -0400

    Conversion y Reverso
    Funcional, first try

--- ferreteria/views.py
+++ ferreteria/views.py
@@ -5,68 +5,12 @@ from urllib.request import urlopen
 from django.shortcuts import render
 
 money_type = [
-    ["USD", "Dólar"],
-    ["SAR", "Rial saudita"],
-    ["ARS", "Peso argentino"],
-    ["AUD", "Dólar australiano"],
-    ["BSP", "Dólar de las Bahamas"],
-    ["BMD", "Dólar de bermudas"],
-    ["BOL", "Boliviano"],
-    ["BRL", "Real"],
-    ["CAD", "Dólar canadiense"],
-    ["QAR", "Riyal catarí"],
-    ["CNY", "Yuan Renminbi"],
-    ["COP", "Peso colombiano"],
-    ["KRW", "Won"],
-    ["CRC", "Colón"],
-    ["CUP", "Peso cubano"],
-    ["DEG", "DEG (FMI)"],
-    ["DKK", "Corona Danesa"],
-    ["EGP", "Libra egipcia"],
-    ["AED", "Dírham"],
-    ["EUR", "Euro"],
-    ["PHP", "Peso filipino"],
-    ["FJD", "Dolár de Fiji"],
-    ["XPF", "Franco"],
-    ["GTQ", "Quetzal guatemalteco"],
-    ["HKD", "Dólar hongkonés"],
-    ["HUF", "Forint"],
-    ["INR", "Rupia India"],
-    ["IDR", "Rupia de Indonesia"],
-    ["IRR", "Rial iraní"],
-    ["ISK", "Corona de islandia"],
-    ["KYD", "Dólar de Islas Caimán"],
-    ["ILS", "Shekel israelí"],
-    ["JPY", "Yen"],
-    ["KZT", "Tenge"],
-    ["MYR", "Ringgit"],
-    ["MAD", "Dírham"],
-    ["MXN", "Peso mexicano"],
-    ["NOK", "Corona noruega"],
-    ["NZD", "Dólar neozelandés"],
-    ["PKR", "Rupia Paquistaní"],
-    ["PAB", "Balboa"],
-    ["PYG", "Guaraní"],
-    ["PEN", "Nuevo sol peruano"],
-    ["PLN", "Zloty"],
-    ["GBP", "Libra esterlina"],
-    ["CZK", "Corona Checa"],
-    ["DOP", "Peso de República Dominicana"],
-    ["RON", "Leu rumano"],
-    ["RUR", "Rublo ruso"],
-    ["SGD", "Dólar de Singapur"],
-    ["ZAR", "Rand"],
-    ["SEK", "Corona sueca"],
-    ["CHF", "Franco suizo"],
-    ["THB", "Baht tailandés"],
-    ["TWD", "Dólar taiwanés"],
-    ["TRY", "Nueva lira turca"],
-    ["UAH", "Hryvnia"],
-    ["UYU", "Peso uruguayo"],
-    ["VEB", "Bolívar"],
-    ["VND", "Dong"],
+    ["USD", "Dólar", "Dolares"],
+    ["KRW", "Won", "Wones"],
+    ["EUR", "Euro", "Euros"],
+    ["JPY", "Yen", "Yenes"],
+    ["GBP", "Libra", "Libras"]
 ]
-
 
 # Funcion
 def fecha_de_hoy():
@@ -82,9 +26,6 @@ def fecha_de_hoy():
     )
     return seteo_fecha
 
-print(fecha_de_hoy())
-
-
 def usd_a_clp(usd):
     values = []
     url = 'https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F073.TCO.PRE.Z.D&firstdate='+str(fecha_de_hoy())+'&lastdate='+str(fecha_de_hoy())
@@ -92,7 +33,7 @@ def usd_a_clp(usd):
     response = urlopen(url)
     data = json.load(response)
     valor = data["Series"]["Obs"][0]["value"]
-    clp = round(float(usd) * float(valor),0)
+    clp = round(float(usd) * float(valor),2)
     values.append(valor)
     values.append(clp)
     return values
@@ -105,13 +46,37 @@ def valor_a_clp(v,m):
     response = urlopen(url)
     data = json.load(response)
     valor = data["Series"]["Obs"][0]["value"]
-    clp = round(float(v) * float(valor),0)
+    clp = round(float(v) * float(valor),2)
     values.append(valor)
     values.append(clp)
     return values
 
+def clp_a_usd(clp):
+    values = []
+    url = 'https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F073.TCO.PRE.Z.D&firstdate='+str(fecha_de_hoy())+'&lastdate='+str(fecha_de_hoy())
+    #url = "https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F073.TCO.PRE.Z.D&firstdate=2024-04-26&lastdate=2024-04-26"
+    response = urlopen(url)
+    data = json.load(response)
+    valor = data["Series"]["Obs"][0]["value"]
+    usd = round(float(clp)/float(valor),2)
+    values.append(valor)
+    values.append(usd)
+    return values
 
-def convertir(request):
+
+def clp_a_valor(v,m):
+    values = []
+    url = "https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F072.CLP."+str(m)+".N.O.D&firstdate="+str(fecha_de_hoy())+"&lastdate="+str(fecha_de_hoy())
+    #url = "https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=ai.arenas@duocuc.cl&pass=K20844763-7&function=GetSeries&timeseries=F072.CLP."+str(m)+".N.O.D&firstdate=2024-04-26&lastdate=2024-04-26"
+    response = urlopen(url)
+    data = json.load(response)
+    valor = data["Series"]["Obs"][0]["value"]
+    divisa = round(float(v)/float(valor),2)
+    values.append(divisa)
+    values.append(clp)
+    return values
+
+def convertir_divisa(request):
     if request.method == "GET":
         return render(request, "llama_conversor.html", {"money_type": money_type})
     elif request.method == "POST":
@@ -146,17 +111,74 @@ def convertir(request):
             values = usd_a_clp(value)
             actual = values[0]
             clp = values[1]
-            return render(request, "converter.html", {"clp": clp, "actual": actual})
+            return render(request, "converter.html", {"convertido": clp, "actual": actual, "moneda" : "Pesos"})
         else:
             for i in range(0, len(money_type)):
                 if money_type[i][0] == tipo_cambio:
                     values = valor_a_clp(value,tipo_cambio)
                     actual = values[0]
                     clp = values[1]
-            return render(request, "converter.html", {"clp": clp, "actual": actual})
+            return render(request, "converter.html", {"convertido": clp, "actual": actual, "moneda" : "Pesos"})
     else:
         return HttpResponse(status=405)
 
+def reconvertir_divisa(request):
+    if request.method == "GET":
+        return render(request, "llama_conversor.html", {"money_type": money_type})
+    elif request.method == "POST":
+        value_str = request.POST.get("clp")
+        tipo_cambio = request.POST.get("moneda_reversa")
+        if not value_str:
+            return render(
+                request,
+                "llama_conversor.html",
+                {"mensaje": "El campo no puede estar vacío."},
+                {"money_type": money_type},
+            )
+        try:
+            # Verifica Numero
+            value = float(value_str)
+        except ValueError:
+            return render(
+                request,
+                "llama_conversor.html",
+                {"mensaje": "Por favor, ingrese un número válido."},
+                {"money_type": money_type},
+            )
+        if value < 0:
+            # Verifica Positivo
+            return render(
+                request,
+                "llama_conversor.html",
+                {"mensaje": "El número no puede ser negativo."},
+                {"money_type": money_type},
+            )
+        if tipo_cambio == "USD":
+            values = clp_a_usd(value)
+            actual = values[0]
+            divisa = values[1]
+            for i in range (len(money_type)):
+                if tipo_cambio == money_type[i][0]:
+                    moneda = money_type[i][2]
+            return render(request, "converter.html", {"convertido": divisa, "actual": actual, "moneda":moneda})
+        else:
+            for i in range(0, len(money_type)):
+                if money_type[i][0] == tipo_cambio:
+                    values = valor_a_clp(value,tipo_cambio)
+                    actual = values[0]
+                    divisa = values[1]
+            for i in range (len(money_type)):
+                if tipo_cambio == money_type[i][0]:
+                    moneda = money_type[i][2]
+            return render(request, "converter.html", {"convertido": divisa, "actual": actual, "moneda":moneda})
+    else:
+        return HttpResponse(status=405) 
+    
+def index(request):
+    return render(
+        request,
+        "index.html",
+    )
 
 def index(request):
     return render(
